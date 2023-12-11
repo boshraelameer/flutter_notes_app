f803225e3be9ca88e34091ef11dfdf1188450903
boshraelameer <133707000+boshraelameer@users.noreply.github.com>
2023-12-11 16:56:55 +0200

    create edit note

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.dark,
         // fontFamily: 'assets/fonts/Poppins-Regular.ttf',
--- lib/view/widgets/note_item.dart
+++ lib/view/widgets/note_item.dart
@@ -1,33 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/view/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: Colors.yellow, borderRadius: BorderRadius.circular(16)),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(color: Colors.black, fontSize: 26),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNoteView();
+        }));
+      },
+      child: Container(
+        padding: const EdgeInsets.only(
+          top: 24,
+          bottom: 24,
+          left: 16,
+        ),
+        decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 252, 204, 128),
+            borderRadius: BorderRadius.circular(16)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
+                style: TextStyle(color: Colors.black, fontSize: 26),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16, bottom: 16),
+                child: Text(
+                  'build your career with our note app',
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(.5), fontSize: 18),
+                ),
+              ),
+              trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.delete,
+                    color: Colors.black,
+                    size: 24,
+                  )),
             ),
-            subtitle: const Text(
-              'build your career',
-              style: TextStyle(color: Colors.black),
+            Padding(
+              padding: const EdgeInsets.only(left: 200),
+              child: Text(
+                'May 21 , 2022',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(.4),
+                  fontSize: 16,
+                ),
+              ),
             ),
-            trailing:
-                IconButton(onPressed: () {}, icon: const Icon(Icons.delete)),
-          ),
-          const Text(
-            'May 21 , 2022',
-            style: TextStyle(color: Colors.black),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
